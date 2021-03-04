267d2cb4a78d601098302d2724ef1533effe6025
Administrator <185079325479@163.com>
2021-03-04 23:15:07 +0800

    123

--- src/main/java/com/bjpowernode/testGit.java
+++ src/main/java/com/bjpowernode/testGit.java
@@ -3,8 +3,8 @@ package com.bjpowernode;
 public class testGit {
     public static void main(String[] args) {
         System.out.println("......");
-        {
+
             System.out.println("......");
-        }
+
     }
 }
